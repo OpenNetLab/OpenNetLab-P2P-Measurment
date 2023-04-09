7a8be5d97c9edcf6726f319541734203cddcdcbd
wangyf2001 <64457943+wangyf2001@users.noreply.github.com>
2023-04-09 22:30:03 +0800

    Update bwmursever.py

--- bwmur/bwmursever.py
+++ bwmur/bwmursever.py
@@ -1,14 +1,17 @@
+import paramiko
 import time
 import random
 import json
 import os
 import sys
+from scp import SCPClient
+import numpy as np
 import copy
 import time
 
 socket_timeout_sec = 240
 machines_file = "machines.json"
-sever_port = "8020"
+sever_port = "8000"
 sever_ip = "20.81.187.38"
 recv_wd = "/home/wang/testnet/"
 
@@ -32,14 +35,8 @@ def get_bw(name):
         machines = json.loads(f.read())
         if name not in machines:
             raise ValueError("Not find such mahcine")
-
-        #print("iperf")
-        #print(machines[name]["host"])
-        #print(machines[name]["bw_port"])
         sever_ip = machines[name]["host"]
         sever_port = machines[name]["bw_port"]
-        
-
         
 def get_ssh(name):
     client = paramiko.SSHClient()
@@ -51,15 +48,9 @@ def get_ssh(name):
 
         client.connect(hostname=machines[name]["host"], port=machines[name]["ssh_port"], username=machines[name]["user"],
                        password=machines[name]["pwd"])
-
-        #print(machines[name]["host"])
-        #print(machines[name]["ssh_port"])
-        #print(machines[name]["user"])
-
     return client
 
 def BWmeasure(matches_num):
-    #print(get_datetime(), "start measure")
     try:
         output =" "
         get_bw("recv_%d" % (matches_num))
@@ -71,19 +62,9 @@ def BWmeasure(matches_num):
         doc.seek(0)
         doc.truncate()
         doc.write(status)
-        
         doc.close()
-        '''
-        netctr = get_ssh("netctr")
-        print("start scp")
-        scp_client = SCPClient(netctr.get_transport(), socket_timeout=30.0)
-        scp_client.put("severbwlog.txt", "%s/." % (recv_wd))
-        scp_client.close()
-        '''
-
     except Exception as e:
         print(get_datetime(), "run_measure", e)
-
     finally:
         print("measure finish")
 
